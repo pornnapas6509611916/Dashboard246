62be181afe179094a874564fbb59baf4a6806304
pornnapas6509611916 <145179458+pornnapas6509611916@users.noreply.github.com>
2024-04-14 11:11:52 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -15,11 +15,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-#to use thai font
-!wget -q https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf
-mpl.font_manager.fontManager.addfont("thsarabunnew-webfont.ttf")
-mpl.rc('font',family="TH Sarabun New",size = '16')
-
 st.set_page_config(
     page_title="Dashboard",
     page_icon="🪸",
