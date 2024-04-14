d9d5a33411824538d40cf8481bc6c346e888ba84
pornnapas6509611916 <145179458+pornnapas6509611916@users.noreply.github.com>
2024-04-14 21:20:22 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -134,7 +134,7 @@ def make_donut(input_df, input_population, input_Satisfaction):
 
     return donut_chart
 
-col = st.columns((1.5, 4.5, 2), gap='medium')
+col = st.columns((4.5, 0.5, 4.5), gap='medium')
 
 with col[0]:
     st.markdown('#### Satisfaction')
